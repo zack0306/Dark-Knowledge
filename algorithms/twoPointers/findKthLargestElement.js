a0f60d1c98bded271903d7ed9a564509d659ac74
Ziyi <crazywings1991@gmail.com>
2019-01-20 19:12:27 -0800

    lint code

--- algorithms/twoPointers/findKthLargestElement.js
+++ algorithms/twoPointers/findKthLargestElement.js
@@ -3,7 +3,8 @@
 // Example
 // In array [9,3,2,4,8], the 3rd largest element is 4.
 
-// In array [1,2,3,4,5], the 1st largest element is 5, 2nd largest element is 4, 3rd largest element is 3 and etc.
+// In array [1,2,3,4,5], the 1st largest element is 5, 2nd largest element is 4, 3rd 
+// largest element is 3 and etc.
 
 //
 // @param n: An integer
@@ -11,44 +12,45 @@
 // @return: the Kth largest element
 //
 
-const kthLargestElement = function (n, nums) {
-    // write your code here
-    if (nums === null) {
-      return -1;
-    }
+const partition = (nums, start, end, n) => {
+  const arr = nums;
+  if (start === end) {
+    return arr[start];
+  }
 
-    return partition(nums, 0, nums.length - 1, n);
+  let left = start;
+  let right = end;
+  const pivot = arr[Math.floor((start + end) / 2)];
+
+  while (left <= right) {
+    while (left <= right && arr[left] < pivot) {
+      left += 1;
+    }
+    while (left <= right && arr[right] > pivot) {
+      right -= 1;
+    }
+    if (left <= right) {
+      const temp = arr[left];
+      arr[left] = arr[right];
+      arr[right] = temp;
+      left += 1;
+      right -= 1;
+    }
+  }
+  if (arr.length - n <= right) {
+    return partition(arr, start, right, n);
+  }
+  if (arr.length - n >= left) {
+    return partition(arr, left, end, n);
+  }
+  return arr[left - 1];
 };
 
-const partition = function (nums, start, end, n){
-    if (start === end) {
-        return nums[start];
-    }
+const kthLargestElement = (n, nums) => {
+  // write your code here
+  if (nums === null) {
+    return -1;
+  }
 
-    let left = start;
-    let right = end;
-    let pivot = nums[Math.floor((start + end) / 2)];
-
-    while (left <= right) {
-        while (left <= right && nums[left] < pivot) {
-            left++;
-        }
-        while (left <= right && nums[right] > pivot) {
-            right --;
-        }
-        if (left <= right) {
-            const temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] =  temp;
-            left++;
-            right--;
-        }
-    }
-    if (nums.length - n <= right) {
-        return partition(nums, start, right, n);
-    }
-    if (nums.length - n >= left) {
-        return partition(nums, left, end, n);
-    }
-    return nums [left - 1];
+  return partition(nums, 0, nums.length - 1, n);
 };
